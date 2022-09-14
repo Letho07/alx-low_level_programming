d9ff1d2308b1817baa344be3cf2a372bbc975755
Letho07 <lethukukhanyahadebe@gmail.com>
2022-09-14 02:08:35 -0700

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,14 @@
+#include <unistd.h>
+#include "main.h"
 /**
-*main- prints _putchar
-*return - 0 (success)
+ * main - prints _putchar
+* return 0 
 */
 int main(void)
 {
-char word[8]="_putchar";
+char word[9] = "_putchar";
 int i;
-for (i=0;i<8;i++)
+for (i = 0; i < 9; i++)
 _putchar(word[i]);
 _putchar('\n');
 return (0);
